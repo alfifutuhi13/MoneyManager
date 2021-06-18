f6774c557ece1310033d7f0e9c1b4eef0725d570
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-06-18 10:35:24 +0700

    Creating BaseController.cs

---
+++ MoneyManager/API/Base/BaseController.cs
@@ -0,0 +1,93 @@
+﻿using API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<Entity, Repository, TId> : ControllerBase
+        where Entity : class
+        where Repository : IGenericRepository<Entity, TId>
+    {
+        private readonly Repository repository;
+        public BaseController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                var get = repository.GetAll();
+                return Ok(get);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.InnerException);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(TId id)
+        {
+            try
+            {
+                var getById = repository.GetById(id);
+                return Ok(getById);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.InnerException);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(Entity entity)
+        {
+            try
+            {
+                var result = repository.Post(entity) > 0 ? (ActionResult)Ok("Data successfully inserted") : BadRequest("Data cannot be inserted") ;
+                return result;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(TId id)
+        {
+            try
+            {
+                var result = repository.Delete(id) > 0 ? (ActionResult)Ok("Data has been deleted") : BadRequest("Data cannot be deleted");
+                return result;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put(Entity entity)
+        {
+            try
+            {
+                var result = repository.Put(entity) > 0 ? (ActionResult)Ok("Data successfully updated") : BadRequest("Data cannot be updated");
+                return result;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+    }
+}
